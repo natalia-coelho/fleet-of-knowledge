7fade9a334825aef3530bbe3c954cb182711b98c
natalia-coelho <nataliacoelho199@gmail.com>
2025-01-19 10:26:29 -0300

    refactor: increment vehicle's repository

--- Interfaces/IVeiculoRepository.cs
+++ Interfaces/IVeiculoRepository.cs
@@ -4,5 +4,8 @@ namespace fleet_of_knowledge.Interfaces;
 
 public interface IVeiculoRepository
 {
+    Task<Veiculo> DeleteVehicle(Guid id);
     Task<Veiculo> GetVehicle();
+    Task<Veiculo> InsertVehicle(Veiculo veiculo);
+    Task<Veiculo> UpdateVehicle(Veiculo veiculo, Guid id);
 }
