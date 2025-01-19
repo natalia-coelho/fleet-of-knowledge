e4d70b744ff466c7adc512412df4474b2dfb6438
natalia-coelho <nataliacoelho199@gmail.com>
2025-01-19 10:25:00 -0300

    feat: add vehicle model

---
+++ Models/Veiculo.cs
@@ -0,0 +1,17 @@
+﻿namespace fleet_of_knowledge.Models;
+
+public class Veiculo
+{
+    public Guid Id { get; set; }
+    public string Placa { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public double Quilometragem { get; set; }
+    public Status Status { get; set; }
+}
+
+public enum Status
+{
+    Ativo,
+    Desativado
+}
