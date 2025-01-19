c9f33ef953c89fab6e736148cc8cd13f9f16c988
natalia-coelho <nataliacoelho199@gmail.com>
2025-01-19 10:24:31 -0300

    feat: add vehicle services

---
+++ Interfaces/IVeiculoService.cs
@@ -0,0 +1,11 @@
+﻿using fleet_of_knowledge.Models;
+
+namespace fleet_of_knowledge.Interfaces;
+
+public interface IVeiculoService
+{
+    public Task<Veiculo> ObterVeiculo();
+    public Task<Veiculo> Cadastrar(Veiculo veiculo);
+    public Task<Veiculo> Atualizar(Veiculo veiculo, Guid id);
+    public Task<Veiculo> Excluir(Guid id);
+}
