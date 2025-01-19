61b555e0002bebc2b8fa2a876f2ad648badad9da
natalia-coelho <nataliacoelho199@gmail.com>
2025-01-19 10:24:04 -0300

    feat: add vehicle repository structure

---
+++ Interfaces/IVeiculoRepository.cs
@@ -0,0 +1,8 @@
+﻿using fleet_of_knowledge.Models;
+
+namespace fleet_of_knowledge.Interfaces;
+
+public interface IVeiculoRepository
+{
+    Task<Veiculo> GetVehicle();
+}
---
+++ Repository/VeiculoRepository.cs
@@ -0,0 +1,44 @@
+﻿using fleet_of_knowledge.Data;
+using fleet_of_knowledge.Interfaces;
+using fleet_of_knowledge.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace fleet_of_knowledge.Repository;
+
+public class VeiculoRepository : IVeiculoRepository
+{
+    private readonly IMongoCollection<Veiculo> _vehicleCollection;
+
+    public VeiculoRepository(IOptions<DatabaseSettings> databaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            databaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            databaseSettings.Value.DatabaseName);
+
+        this._vehicleCollection = mongoDatabase.GetCollection<Veiculo>(
+            databaseSettings.Value.VehiclesCollectionName);
+    }
+
+    public Task<Veiculo> DeleteVehicle(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Veiculo> GetVehicle()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Veiculo> InsertVehicle(Veiculo veiculo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Veiculo> UpdateVehicle(Veiculo veiculo, Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
