44aa82677bbf0ac89b611e519240d1031e8a6ddb
Bernardo Pires Mesko <bemesko@gmail.com>
2025-01-23 07:00:19 -0300

    Create initial build-env task

--- .dagger/main.go
+++ .dagger/main.go
@@ -3,41 +3,18 @@
 package main
 
 import (
-	"context"
 	"dagger/vehicle-registry-api/internal/dagger"
 )
 
 type VehicleRegistryApi struct{}
 
-// Returns a container that echoes whatever string argument is provided
-func (m *VehicleRegistryApi) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
-}
-
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *VehicleRegistryApi) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
+// Builds a base container with all dependencies installed
+func (m *VehicleRegistryApi) BuildEnv(source *dagger.Directory) *dagger.Container {
+	dotnetCache := dag.CacheVolume("dotnet")
 	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
-}
-
-// Builds the Dockerfile for this repository
-func (m *VehicleRegistryApi) BuildDocker(
-	ctx context.Context,
-	src *dagger.Directory,
-) (string, error) {
-	ref, err := dag.Container().
-		WithDirectory("/src", src).
-		WithWorkdir("/src").
-		Directory("/src").
-		DockerBuild().
-		Publish(ctx, "ttl.sh/bernats/hello-dagger:5m")
-	if err != nil {
-		return "", err
-	}
-
-	return ref, nil
+		From("mcr.microsoft.com/dotnet/sdk:8.0-alpine").
+		WithDirectory("/source", source).
+		WithMountedCache("/root/.nuget/packages", dotnetCache).
+		WithWorkdir("/source").
+		WithExec([]string{"dotnet", "restore"})
 }
